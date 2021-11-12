2a4bf4a7b10952ebf4090f7acd698d95ca4fed76
karla-walker <indiablonix@gmail.com>
2021-11-11 17:31:10 -0800

    Add watch target and passthrough copy of font and img folders

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./src/sass/');
   eleventyConfig.addPassthroughCopy('./src/css');
+  eleventyConfig.addWatchTarget('./src/fonts/');
+  eleventyConfig.addPassthroughCopy('./src/fonts');
+  eleventyConfig.addWatchTarget('./src/img/');
+  eleventyConfig.addPassthroughCopy('./src/img');
 
   return {
     // define directories
